2ae2e18c3ca6d6dfa85d3af2734fcffdfd50291a
Andrew Huynh <a5thuynh@gmail.com>
2018-04-12 12:49:08 -0700

    Fixing hostname in test

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-response=$(curl --write-out %{http_code} --silent --output /dev/null servername)
+response=$(curl --write-out %{http_code} --silent --output /dev/null localhost:4180)
 expected="403"
 
 echo "Expecting: $expected"
 echo "Server says: $response"
 
 if [ "$expected" != "$response" ]; then
-  echo "Test failed"
+  echo "Test failed!"
   exit 1
 else
-  echo "Test passed"
+  echo "Test passed!"
   exit 0
 fi
