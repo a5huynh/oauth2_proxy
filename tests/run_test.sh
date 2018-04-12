00b7d9f7caf07f3853fb656cea74d07700c6c7b4
Andrew Huynh <a5thuynh@gmail.com>
2018-04-12 12:25:39 -0700

    Adding simple test to check response code

---
+++ tests/run_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+response=$(curl --write-out %{http_code} --silent --output /dev/null servername)
+expected="403"
+
+echo "Expecting: $expected"
+echo "Server says: $response"
+
+if [ "$expected" != "$response" ]; then
+  echo "Test failed"
+  exit 1
+else
+  echo "Test passed"
+  exit 0
+fi
